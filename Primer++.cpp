b2b2bd4f611715a06ed3aab81208eda007f24d96
Shiraz <24913281+smcclennon@users.noreply.github.com>
2020-05-07 23:27:41 +0100

    Bug fixes, preparing for cross-compatibility

--- Primer++.cpp
+++ Primer++.cpp
@@ -1,4 +1,4 @@
-// Primer
+// Primer++
 // github.com/smcclennon/Primer
 
 // Started porting Primer.py on the 5th may 2020
@@ -13,31 +13,44 @@
 #include <map>
 #include "json.hpp"
 
+// OS specific features
+#ifdef __linux__
+    #include <unistd.h>
+#elif _WIN32
+    #include "windows.h"
+#endif
 
 
 using json = nlohmann::json;
 
 std::string ver = "1.0.0";
-std::string proj = "Primer";
+std::string proj = "Primer++";
 
+// Initialise variables
+bool debug = false;
+bool invalid = false;
 int found = 0;
 int calculations = 0;
-bool invalid = false;
-bool debug = false;
 double task_duration;
 std::clock_t task_start;
 
-std::fstream config_file("Primer_config.json");
+
 json json_config;
+std::ofstream config_file;
+std::ofstream txt_file_append;
 
 
 
-int latest_prime() { return json_config["statistics"]["Latest Prime"].get<int>(); }
-int primes_found() { return json_config["statistics"]["Primes Found"].get<int>(); }
-int total_calculations() { return json_config["statistics"]["Total Calculations"].get<int>(); }
+// Allow the largest possible positive integer to be stored (max: 18,446,744,073,709,551,615 *or greater)
+// * the actual value depends on the particular system and library implementation, but shall reflect the limits of these types in the target platform.
+// https://www.cplusplus.com/reference/climits/
+unsigned long long latest_prime() { return json_config["statistics"]["Latest Prime"].get<unsigned long long>(); }
+unsigned long long primes_found() { return json_config["statistics"]["Primes Found"].get<unsigned long long>(); }
+unsigned long long total_calculations() { return json_config["statistics"]["Total Calculations"].get<unsigned long long>(); }
 
+// Format numbers with commas (1234567 -> 1,234,567)
 // https://stackoverflow.com/a/24192835
-std::string insert_commas(int num)
+std::string insert_commas(unsigned long long num)
 {
     std::string numWithCommas = std::to_string(num);
     int insertPosition = numWithCommas.length() - 3;
@@ -49,6 +62,8 @@ std::string insert_commas(int num)
     return numWithCommas;
 }
 
+
+
 int main()
 {
     using std::cout;
@@ -56,19 +71,39 @@ int main()
 
 
 
+    cout << proj << " " << ver << " - "
+         << "A prime number generator\n"
+         << "github.com/smcclennon/Primer\n"
+         << "\nRewritten from the ground up in C++ (5th - 7th may 2020)"
+         << std::flush;
+
+    #ifdef __linux__
+        usleep(3);
+    #elif _WIN32
+        Sleep(3000);
+    #endif
+
+    cout << endl << endl;
+
+
+
     try {
         if (debug) { cout << "Trying to load config" << endl;
         }
+        std::ifstream config_file("Primer_config.json");
         json_config = json::parse(config_file);
     } catch (...) {
-        cout << "Created Primer_config.json" << endl;
+        cout << "Created Primer_config.json" << endl << endl;
         json_config = "{\"statistics\": {\"Total Calculations\": 0, \"Primes Found\": 0, \"Latest Prime\": 0}, \"debugging\": {\"Force Unix\": \"False\"}}"_json;
     }
 
+    // Convert value types to unsigned long long
+    json_config["statistics"]["Total Calculations"] = total_calculations();
+    json_config["statistics"]["Latest Prime"] = latest_prime();
+    json_config["statistics"]["Primes Found"] = primes_found();
 
-
-    if (debug) { cout << json_config.dump(4) << endl;
-    }
+    /*if (debug) { cout << json_config.dump(4) << endl;
+    }*/
 
     bool new_round = true;
 
@@ -84,7 +119,8 @@ int main()
         }
 
 
-        if (debug) { cout << "\n-- RESTART WHILE TRUE --" << endl;
+        if (debug) { cout << "\n-- RESTART WHILE TRUE --" << endl
+                          << json_config.dump(4) << endl;
         }
 
         while (invalid == false)
@@ -93,8 +129,9 @@ int main()
             if (debug) {
                 cout << "[WHILE] Invalid: false" << endl
                 << "[WHILE] latest_prime = " << latest_prime() << endl
-                << "[WHILE] latest_prime % 2 == 0: ???" << endl
+                //<< "[WHILE] latest_prime % 2 == 0: ???" << endl
                 << "[WHILE] Calculations = " << calculations << endl
+                << "[WHILE] Total  = " << total_calculations() << endl
                 << "--START IF--" << endl;
 
             }
@@ -151,7 +188,7 @@ int main()
                 }
                 json_config["statistics"]["Total Calculations"] = total_calculations() + calculations;
                 json_config["statistics"]["Primes Found"] = primes_found() + 1;
-                cout << proj << "++ v" << ver << "  "
+                cout //<< proj << " " << ver << "  "
                      << "Found Prime [#" << insert_commas(primes_found()) << "]!  "
                      << "-->  " << insert_commas(latest_prime()) << " <--  "
                      << "[Total: " << insert_commas(total_calculations()) << "]  "
@@ -160,24 +197,20 @@ int main()
                 calculations = 0;
 
                 // https://www.cplusplus.com/doc/tutorial/files/
-                std::ofstream config_file("Primer_config.json");
-                std::ofstream txt_file_append("Primer.txt", std::ios::out | std::ios::app);
-
+                //std::ofstream config_file("Primer_config.json", std::ios::trunc);
+                //std::ofstream txt_file_append("Primer.txt", std::ios::out | std::ios::app);
                 int successful_write = 0;
                 while (successful_write < 2) {
-                    if (config_file.is_open()) {
-                        config_file << json_config.dump(4) << std::endl;
-                        config_file.close();
-                        successful_write = 1;
-                    } else {
-                        cout << "Unable to open file \"Primer_config.json\"" << endl; }
+                    config_file.open("Primer_config.json", std::ios::trunc);
+                    config_file << json_config.dump(4) << std::endl;
+                    config_file.close();
+                    successful_write = 1;
 
-                    if (txt_file_append.is_open()) {
-                        txt_file_append << latest_prime() << std::endl;
-                        txt_file_append.close();
-                        successful_write = 2;
-                    } else {
-                        cout << "Unable to open file \"Primer.txt\"" << endl; }
+                    txt_file_append.open("Primer.txt", std::ios::app);
+                    txt_file_append << latest_prime() << std::endl;
+                    txt_file_append.close();
+                    //txt_file_append.close();
+                    successful_write = 2;
                 }
 
                 if (debug) { std::cin.get();
