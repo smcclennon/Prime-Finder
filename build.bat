f7eda3c31b292c52a809ec95eaa38e6f659ebc07
Shiraz <24913281+smcclennon@users.noreply.github.com>
2020-05-10 08:16:39 +0100

    Added -std=c++17 to the build command

--- build.bat
+++ build.bat
@@ -7,6 +7,6 @@ set "resource_file=Primer++.o"
 set "output_file=Primer++.exe"
 
 echo Building %output_file%...
-g++ "%source_file%" "%resource_file%" -o "%output_file%" --static -pedantic -Wall -Wextra -Wcast-align -Wcast-qual -Wctor-dtor-privacy -Wdisabled-optimization -Wformat=2 -Winit-self -Wlogical-op -Wmissing-declarations -Wmissing-include-dirs -Wnoexcept -Wold-style-cast -Woverloaded-virtual -Wredundant-decls -Wshadow -Wsign-conversion -Wsign-promo -Wstrict-null-sentinel -Wstrict-overflow=5 -Wswitch-default -Wundef -Werror -Wno-unused
+g++ "%source_file%" "%resource_file%" -o "%output_file%" --static -std=c++17 -pedantic -Wall -Wextra -Wcast-align -Wcast-qual -Wctor-dtor-privacy -Wdisabled-optimization -Wformat=2 -Winit-self -Wlogical-op -Wmissing-declarations -Wmissing-include-dirs -Wnoexcept -Wold-style-cast -Woverloaded-virtual -Wredundant-decls -Wshadow -Wsign-conversion -Wsign-promo -Wstrict-null-sentinel -Wstrict-overflow=5 -Wswitch-default -Wundef -Werror -Wno-unused
 echo Build complete!
 timeout 3
