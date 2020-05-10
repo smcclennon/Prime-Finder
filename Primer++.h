fec3f8f95d3749a51489d535321173587f45a717
Shiraz <24913281+smcclennon@users.noreply.github.com>
2020-05-10 02:49:59 +0100

    Aid with automated building

---
+++ Primer++.h
@@ -0,0 +1,7 @@
+// Primer++
+// github.com/smcclennon/Primer
+// Simple header to aid with automated building
+
+#include <string>
+std::string primer_ver();
+std::string primer_ver() { return "1.2.2"; }
