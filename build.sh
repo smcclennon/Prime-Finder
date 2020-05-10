28d8d629c4c426e5ecfedb0c04191e2bbb22c1c3
Shiraz <24913281+smcclennon@users.noreply.github.com>
2020-05-10 13:58:58 +0100

    Compatible with MacOS and Linux

--- build.sh
+++ build.sh
@@ -6,8 +6,7 @@
 echo Warning: This build command has only been tested on MacOS Catalina 10.15.4 with Apple clang 11.0.3
 echo This is MacOS only and may not work on other versions of MacOS (and/or) Apple clang
 echo Building...
-# [GCC 4.2.1] g++ Primer++.cpp Primer++.h json.hpp -std=c++11
 # chmod +x build.sh
-clang++ Primer++.cpp -o Primer++ -std=c++17
+gcc++ Primer++.cpp -o Primer++ -std=c++17
 echo Build complete!
 ./Primer++
