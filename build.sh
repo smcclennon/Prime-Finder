cb4ccf39058e79fc6f04da8ce2fc99c3c7fb7a7c
Shiraz <24913281+smcclennon@users.noreply.github.com>
2020-05-10 02:48:19 +0100

    MacOS build script

---
+++ build.sh
@@ -0,0 +1,12 @@
+# Primer++
+# github.com/smcclennon/Primer
+# Tested with MacOS Catalina 10.15.4 with Apple clang 11.0.3
+
+echo Warning: This build command has only been tested on MacOS Catalina 10.15.4 with Apple clang 11.0.3
+echo THis is MacOS only and may not work on other versions of MacOS (and/or) Apple clang
+echo Building...
+# [GCC 4.2.1] g++ Primer++.cpp Primer++.h json.hpp -std=c++11
+chmod +x build.sh
+clang++ Primer++.cpp -o Primer++ -std=c++17
+./Primer++
+echo Build complete!
