9fdf7a73a71d61e949f147df35f719bc6d5e2b0d
Shiraz <24913281+smcclennon@users.noreply.github.com>
2020-05-11 23:28:36 +0100

    Use makefile

--- build.sh
+++ build.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 # Primer++
 # github.com/smcclennon/Primer
-# Tested with MacOS Catalina 10.15.4 with Apple clang 11.0.3
+# Not tested
 
-echo Warning: This build command has only been tested on MacOS Catalina 10.15.4 with Apple clang 11.0.3
-echo This is MacOS only and may not work on other versions of MacOS (and/or) Apple clang
-echo Building...
 # chmod +x build.sh
-gcc++ Primer++.cpp -o Primer++ -std=c++17
-echo Build complete!
-./Primer++
+#gcc++ Primer++.cpp -o Primer++ -std=c++17
+#echo Build complete!
+#./Primer++
+make
+make clean
